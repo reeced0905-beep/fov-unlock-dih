7fdb1d50690530605dd110fdac9b84c193fb129e
reeced0905-beep <reeced0905@gmail.com>
2026-07-05 01:04:26 -0500

    Use explicit parent parameter for ConfigScreenFactory

--- src/main/java/com/mistycalpi/infinizoom/compat/ModMenuIntegration.java
+++ src/main/java/com/mistycalpi/infinizoom/compat/ModMenuIntegration.java
@@ -3,12 +3,11 @@ package com.mistycalpi.infinizoom.compat;
 import com.mistycalpi.infinizoom.config.ZoomConfigScreen;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import net.minecraft.client.gui.screens.Screen;
 
 public class ModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> new ZoomConfigScreen(parent);
+        return (parent) -> new ZoomConfigScreen(parent);
     }
 }
