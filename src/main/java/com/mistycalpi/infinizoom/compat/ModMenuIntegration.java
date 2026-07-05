62b1b8faf4b178256d9142e6deafaea68188a89b
reeced0905-beep <reeced0905@gmail.com>
2026-07-05 01:02:46 -0500

    Fix ModMenuIntegration to properly create ZoomConfigScreen

--- src/main/java/com/mistycalpi/infinizoom/compat/ModMenuIntegration.java
+++ src/main/java/com/mistycalpi/infinizoom/compat/ModMenuIntegration.java
@@ -3,11 +3,12 @@ package com.mistycalpi.infinizoom.compat;
 import com.mistycalpi.infinizoom.config.ZoomConfigScreen;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.minecraft.client.gui.screens.Screen;
 
 public class ModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ZoomConfigScreen::new;
+        return parent -> new ZoomConfigScreen(parent);
     }
 }
