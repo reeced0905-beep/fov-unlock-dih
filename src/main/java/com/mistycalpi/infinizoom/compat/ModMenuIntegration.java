abac212e8e5eaffd67b6d19b43736fb3368ef9c4
reeced0905-beep <reeced0905@gmail.com>
2026-07-05 01:06:23 -0500

    Disable ModMenuIntegration due to Minecraft 26.1.2 compatibility issue

--- src/main/java/com/mistycalpi/infinizoom/compat/ModMenuIntegration.java
+++ src/main/java/com/mistycalpi/infinizoom/compat/ModMenuIntegration.java
@@ -4,10 +4,15 @@ import com.mistycalpi.infinizoom.config.ZoomConfigScreen;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 
-public class ModMenuIntegration implements ModMenuApi {
+// TODO: ModMenu integration disabled due to Minecraft 26.1.2 API incompatibility
+// The ConfigScreenFactory uses obfuscated Minecraft classes that aren't available
+// public class ModMenuIntegration implements ModMenuApi {
+//
+//     @Override
+//     public ConfigScreenFactory<?> getModConfigScreenFactory() {
+//         return (parent) -> new ZoomConfigScreen(parent);
+//     }
+// }
 
-    @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return (parent) -> new ZoomConfigScreen(parent);
-    }
+public class ModMenuIntegration {
 }
